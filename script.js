7b57c269e2395c00f5ba765abde697e9865fbcaa
stoclet sylvain <cronosu@hotmail.fr>
2023-03-23 11:09:04 +0100

    a

---
+++ script.js
@@ -0,0 +1,15 @@
+let etoile = document.getElementsByClassName("etoile");
+
+function entierAleatoire(min, max)
+{
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+for (let i = 0; i < etoile.length; i++) {
+  etoile[i].style.left = entierAleatoire(-49, 49) + "%";
+  etoile[i].style.bottom = entierAleatoire(-49, 49) + "%";
+  console.log(etoile[i].style.left + " " + etoile[i].style.bottom);
+  
+};
+console.log(etoile.length);
+
