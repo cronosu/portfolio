422e3fb55d00eecb8239de9780b5e51247bec205
stoclet sylvain <cronosu@hotmail.fr>
2023-04-19 00:03:18 +0200

    a

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
 let etoile = document.getElementsByClassName("etoile");
 
-function entierAleatoire(min, max)
+function  entierAleatoire(min, max)
 {
  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -14,4 +14,28 @@ for (let i = 0; i < etoile.length; i++) {
 };
 console.log(etoile.length);
 });
+
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowRight') {
+    
+    window.scrollBy(-10, 0);
+  } else if (event.key === 'ArrowLeft') {
+
+    window.scrollBy(10, 0);
+  }
+});
+
+
+document.addEventListener("scroll", function() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  var topValue = 1 + scrollTop + "%"; // Remplacez 1 par la valeur de "top" initiale souhaitée
+  document.querySelector(".element").style.top = topValue;
+});
+
+console.log(window.pageYOffset);
+
+
+
+
 
